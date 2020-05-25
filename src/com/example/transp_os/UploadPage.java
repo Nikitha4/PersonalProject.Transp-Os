6e6bca9569ad2ecbc390d4ff9beec66d0d000bcc
Nikitha Nagumalli <nikitha4@illinois.edu>
2020-05-25 16:50:07 -0500

    able to go back to home screen from UploadPage

--- src/com/example/transp_os/UploadPage.java
+++ src/com/example/transp_os/UploadPage.java
@@ -2,13 +2,17 @@ package com.example.transp_os;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class UploadPage {
 
+    private static JFrame frame;
     private JPanel Upload;
+    private JButton backButton;
 
     public static void NewScreen() {
-        JFrame frame = new JFrame("Transp-Os");
+        frame = new JFrame("Transp-Os");
         frame.setContentPane(new UploadPage().Upload);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Dimension dimension = new Dimension();
@@ -17,5 +21,16 @@ public class UploadPage {
         frame.setMinimumSize(dimension);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public UploadPage() {
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                App mainScreen = new App();
+                mainScreen.NewScreen();
+                frame.setVisible(false);
+            }
+        });
     }
 }
